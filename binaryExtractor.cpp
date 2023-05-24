cd69a6ff580eddb1440d85d0adcbe160ed112e3a
Lowly <87949654+L0w1y@users.noreply.github.com>
2023-05-24 09:05:09 +0700

    Update binaryExtractor.cpp
    Changelog:
     - Updated script file
     - Removed unresolved references
     - Code is updated

--- binaryExtractor.cpp
+++ binaryExtractor.cpp
@@ -1,48 +1,46 @@
-#include "header.h"
+#include "binaryExtractor.h"
 
-using namespace std;
-
-BinaryExtractor::BinaryExtractor(const std::string& filename) : m_filename(filename) {}
+BinaryExtractor::BinaryExtractor(const string& filename) : m_filename(filename) {}
 
 void BinaryExtractor::extract() {
-    std::string folder = m_filename.substr(0, m_filename.find_last_of('.'));
+    string folder = m_filename.substr(0, m_filename.find_last_of('.'));
     mkDIR(folder);
 
-    std::ifstream input(m_filename, std::ios::binary);
+    ifstream input(m_filename, std::ios::binary);
     if (!input.is_open()) {
-        std::cerr << "Error: unable to open file " << m_filename << std::endl;
+        cerr << "Error: unable to open file " << m_filename << std::endl;
         return;
     }
 
-    input.seekg(0, std::ios::end);
+    input.seekg(0, ios::end);
     size_t file_size = input.tellg();
-    input.seekg(0, std::ios::beg);
+    input.seekg(0, ios::beg);
 
     while (input.tellg() < file_size) {
-        std::string name = readcstr(input);
-        std::string ftype = readcstr(input);
+        string name = readcstr(input);
+        string ftype = readcstr(input);
         uint32_t c = readuint32(input);
         uint32_t d = readuint32(input);
         uint32_t e = readuint32(input);
-        std::vector<unsigned char> data(d);
+        vector<unsigned char> data(d);
         input.read(reinterpret_cast<char*>(data.data()), d);
         if (c & 1) {
             data = decompress(data, e);
         }
-        std::string output_filename = folder + "/" + name;
-        std::ofstream output(output_filename, std::ios::binary);
+        string output_filename = folder + "/" + name;
+        ofstream output(output_filename, ios::binary);
         if (!output.is_open()) {
-            std::cerr << "Error: unable to create file " << output_filename << std::endl;
+            cerr << "Error: unable to create file " << output_filename << endl;
             return;
         }
         output.write(reinterpret_cast<const char*>(data.data()), data.size());
         output.close();
-        std::cout << name << " " << ftype << std::endl;
+        cout << name << " " << ftype << endl;
     }
-    std::cout << "COMPLETED!!!" << std::endl;
+    cout << "COMPLETED!!!" << endl;
 }
 
-void BinaryExtractor::mkDIR(const std::string& dir) {
+void BinaryExtractor::mkDIR(const string& dir) {
     #ifdef _WIN32
         _mkdir(dir.c_str());
     #else 
@@ -50,28 +48,28 @@ void BinaryExtractor::mkDIR(const std::string& dir) {
     #endif
 }
 
-std::vector.unsigned char> BinaryExtractor::decompress(const std::vector<unsigned char>& input, size_t output_size) {
-    std::vector<unsigned char> output(output_size);
+vector.unsigned char> BinaryExtractor::decompress(const vector<unsigned char>& input, size_t output_size) {
+    vector<unsigned char> output(output_size);
     unsigned long output_size_long = static_cast<unsigned long>(output_size);
     unsigned long input_size_long = static_cast<unsigned long>(input.size());
     int zret = uncompress(output.data(), &output_size_long, input.data(), input_size_long);
     if (zret != Z_OK) {
-        throw std::runtime_error("Error: decompression failed");
+        throw runtime_error("Error: decompression failed");
     }
     return output;
 }
 
-uint32_t BinaryExtractor::readuint32(std::ifstream& input) {
+uint32_t BinaryExtractor::readuint32(ifstream& input) {
     uint32_t value;
     input.read(reinterpret_cast<char*>(&value), sizeof(uint32_t));
     return value;
 }
 
-std::string BinaryExtractor::readcstr(std::ifstream& input) {
-    std::vector<char> buffer;
+string BinaryExtractor::readcstr(ifstream& input) {
+    vector<char> buffer;
     char c;
     while (input.read(&c, 1) && c != '\0') {
         buffer.push_back(c);
     }
-    return std::string(buffer.begin(), buffer.end());
+    return string(buffer.begin(), buffer.end());
 }
